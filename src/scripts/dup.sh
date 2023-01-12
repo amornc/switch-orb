4fcff1c903641e4a2c34527b6c05a35b93b6759e
amornc <mornster@gmail.com>
2023-01-12 13:29:50 +0700

    Update dup.sh

--- src/scripts/dup.sh
+++ src/scripts/dup.sh
@@ -2,12 +2,12 @@
 
 compare=$(grep -F -f circleci-repositories.yaml github-actions-repositories.yaml)
 export compare
-if [[ -z $compare ]]; then
-  echo $compare
+if [[ -z "$compare" ]]; then
+  echo "$compare"
   echo "Repositories are not duplicates"
   exit 0
 else
-  echo $compare
+  echo "$compare"
   echo "Repositories are duplicates"
   exit 1
 
