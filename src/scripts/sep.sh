2f76e37d2ecf13b4ad57645d141167c904f80f9a
amornc <mornster@gmail.com>
2023-01-12 13:24:04 +0700

    feat: "fix" human error

--- src/scripts/sep.sh
+++ src/scripts/sep.sh
@@ -3,7 +3,7 @@
 yq '.repositories.provider.github-actions[]' "${PARAM_DIR}"/"${PARAM_SEP}" > github-actions-repositories.yaml
 yq '.repositories.provider.circleci[]' "${PARAM_DIR}"/"${PARAM_SEP}" > circleci-repositories.yaml
 echo -------------------------------- Repositories are run on CircleCI --------------------------------
-cat github-actions-repositories.yaml
-echo -------------------------------- Repositories are run on Actions --------------------------------
 cat circleci-repositories.yaml
+echo -------------------------------- Repositories are run on Actions --------------------------------
+cat github-actions-repositories.yaml
 echo -------------------------------------------------------------------------------------------------
