e6807fda2f60b00955b27b3d5736f26cc5f71766
amornc <mornster@gmail.com>
2023-01-12 12:39:10 +0700

    feat: "fix" issue1

--- src/scripts/seperate.sh
+++ src/scripts/seperate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-yq '.repositories.provider.github-actions[]' ${PARAM_DIR}/${PARAM_SEP} > github-actions-repositories.yaml
-yq '.repositories.provider.circleci[]' ${PARAM_DIR}/${PARAM_SEP} > circleci-repositories.yaml
+yq '.repositories.provider.github-actions[]' "${PARAM_DIR}"/"${PARAM_SEP}" > github-actions-repositories.yaml
+yq '.repositories.provider.circleci[]' "${PARAM_DIR}"/"${PARAM_SEP}" > circleci-repositories.yaml
 echo -------------------------------- Repositories are run on CircleCI --------------------------------
 cat github-actions-repositories.yaml
 echo -------------------------------- Repositories are run on Actions --------------------------------
